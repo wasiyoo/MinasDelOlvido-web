d4cb5d660425dabec7ea552090c2f9bb1a24bd74
wilia <wilia@users.noreply.github.com>
2026-07-17 13:15:37 -0400

    Agregar joystick de mira y lupa de precision

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784304431|11258407';
+const CACHE_VERSION = '1784307988|22515372';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
