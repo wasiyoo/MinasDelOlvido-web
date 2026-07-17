bd3ed8a240e8a6df362126ceafc90b568f022295
wilia <wilia@users.noreply.github.com>
2026-07-17 12:12:23 -0400

    Corregir arrastre de controles móviles

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784303358|10904105';
+const CACHE_VERSION = '1784304431|11258407';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
