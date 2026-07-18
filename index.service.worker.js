af68712f6e27506f0b700daee46333f9c9517349
wilia <wilia@users.noreply.github.com>
2026-07-17 22:40:37 -0400

    Restaurar todos los assets premium

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784337825|20835483';
+const CACHE_VERSION = '1784342238|284293104';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
