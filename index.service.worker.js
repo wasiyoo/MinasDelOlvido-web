337e12b0aeb1524b75f1f2ec4b3cb028ae7cf4d7
wilia <wilia@users.noreply.github.com>
2026-07-17 23:16:27 -0400

    Mejora relieve y galerías del Cerro Rico

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784342238|284293104';
+const CACHE_VERSION = '1784344072|30829577';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
