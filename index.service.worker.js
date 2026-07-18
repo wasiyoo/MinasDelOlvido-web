96db47780fd7090d509e562d5296e25fbd04c706
wilia <wilia@users.noreply.github.com>
2026-07-18 01:44:54 -0400

    Añade progresión de profundidad y bosses

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784347813|29755137';
+const CACHE_VERSION = '1784353108|162666083';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
