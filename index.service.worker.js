fc3299b113800c99153f338e17d0da514c5fa535
wilia <wilia@users.noreply.github.com>
2026-07-19 02:53:21 -0400

    Añade siete bosses del folclore boliviano

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784353108|162666083';
+const CACHE_VERSION = '1784443908|37753917';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
