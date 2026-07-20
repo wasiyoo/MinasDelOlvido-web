0cc2cc49e2b56aa88e7950b90e912ba7e28a0a60
wilia <wilia@users.noreply.github.com>
2026-07-20 12:39:38 -0400

    Actualiza bosses, checkpoint, NPCs y protagonista

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784525768|61044157';
+const CACHE_VERSION = '1784565281|145769725';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
