9cf71c4969df503bb72db981f88428ee82395f81
wilia <wilia@users.noreply.github.com>
2026-07-21 10:32:34 -0400

    Implementar nueva interfaz de mochila

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784617972|109559147';
+const CACHE_VERSION = '1784643993|98940747';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
