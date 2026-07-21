ae63b1f883d2e9a0a6892a369cc29dfc7b1de4a1
wilia <wilia@users.noreply.github.com>
2026-07-21 01:08:01 -0400

    Corrige checkpoint y renueva interfaces de carga

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784565281|145769725';
+const CACHE_VERSION = '1784610050|158278378';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
