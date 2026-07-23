aae34ed40f7aa5595d2dab254481d5230e837e62
wilia <wilia@users.noreply.github.com>
2026-07-23 12:18:41 -0400

    Mejora ajustes pausa herramientas y combate

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784738408|39417399';
+const CACHE_VERSION = '1784822702|59421756';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
