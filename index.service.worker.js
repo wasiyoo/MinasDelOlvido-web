3375a9d09bbaaef4508eb6acf64d8476c6f94717
wilia <wilia@users.noreply.github.com>
2026-07-24 12:59:38 -0400

    Agrega guia inicial de la ruta del socavon

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784908251|123884007';
+const CACHE_VERSION = '1784911202|32144048';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
