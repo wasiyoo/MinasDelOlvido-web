2482de6ba74d90e3e76e3ff19c481c8d30be2d23
wilia <wilia@users.noreply.github.com>
2026-07-25 22:13:51 -0400

    Limpia recursos temporales del export web

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785030370|33237026';
+const CACHE_VERSION = '1785031897|31760472';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
