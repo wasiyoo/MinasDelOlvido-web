5e9207ade364eabec86ce907bb42bd49510ea708
wilia <wilia@users.noreply.github.com>
2026-07-26 00:45:13 -0400

    Anima herramientas y renueva cofres del socavón

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785031897|31760472';
+const CACHE_VERSION = '1785038290|61908521';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
