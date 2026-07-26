4e939f5e0460ba83037e7092b7344bec3813a64a
wilia <wilia@users.noreply.github.com>
2026-07-26 02:26:56 -0400

    Asegura uso tactil de pociones y lanzables

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785046324|38804068';
+const CACHE_VERSION = '1785046987|31969624';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
