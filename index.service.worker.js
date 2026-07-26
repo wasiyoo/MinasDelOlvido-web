9dcfefac37b4b945853e973ddaf341d34a435fb6
wilia <wilia@users.noreply.github.com>
2026-07-26 02:15:20 -0400

    Mejora fisica, lanzables y pociones

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785038290|61908521';
+const CACHE_VERSION = '1785046324|38804068';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
