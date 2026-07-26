6b70d9c2e5d0605b2f893eeabd8247b6f419f164
wilia <wilia@users.noreply.github.com>
2026-07-25 20:31:15 -0400

    Corrige progreso y ciclo de misiones de jefes

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785010517|32429032';
+const CACHE_VERSION = '1785025224|37380052';
 /** @type {string} */
 const CACHE_PREFIX = 'Minas del Olvido-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
